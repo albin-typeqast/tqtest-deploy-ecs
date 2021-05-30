8847c4074da19eb72ed8a914eabfc4b347047f3c
Albin Korbar <akorbar@schubergphilis.com>
2021-05-30 20:16:43 +0200

    nicer deploy-ecs-ecr-jenkins.sh script

--- deploy-ecs-ecr-jenkins.sh
+++ deploy-ecs-ecr-jenkins.sh
@@ -1,2 +1,50 @@
-aws cloudformation deploy --template-file ./EcsClusterStack --stack-name EcsClusterStack --parameter-overrides KeyName=tqtest-albin --tags 'Key=env Value=dev' 'Key=app Value=tqtest-ecs-hello-world' --capabilities CAPABILITY_IAM --profile=tq-dev --region=eu-west-1 && aws cloudformation deploy --template-file ./Jenkins --stack-name Jenkins --parameter-overrides KeyName=tqtest-albin --tags 'Key=env Value=dev' 'Key=app Value=jenkins-server' --capabilities CAPABILITY_IAM --profile=tq-dev --region=eu-west-1 && aws ecr create-repository --repository-name tqtest-ecr-hello-world --tags 'Key=env Value=dev' 'Key=app Value=tqtest-ecs-hello-world' --profile=tq-dev --region=eu-west-1
+#!/bin/bash
 
+#
+# simple script calling AWS API to create resources through AWS CLI tool
+#
+# requirements:
+#	- AWS CLI tool installed
+# 	- AWS credentials for AWS CLI configured
+# 	- KeyPair generated for EC2 instances
+#
+# adjust variables per need: 
+#	- $REGION 
+#	- $PROFILE if using profiles when authenticating with AWS API
+#	- $KEYPAIR
+
+
+# General variables
+REGION="eu-west-1"
+PROFILE="tq-dev"
+# KeyPair used to connect to instances
+KEYPAIR="tqtest-albin"
+
+# CloudFormation stack names
+ECSCLUSTER_STACKNAME="EcsClusterStack"
+JENKINS_STACKNAME="Jenkins"
+
+# ECR repository name
+ECRREPO="tqtest-ecr-heello-world"
+
+
+# set profile switch for AWS commands if $PROFILE variable is not ""
+if [[ $PROFILE == "" ]]; then
+	PROFILESWITCH=""
+else
+	PROFILESWITCH="--profile=$PROFILE"
+fi
+
+# create ECS cluster composed of two EC2 instances from ./EcsClusterStack cloudformation template
+aws cloudformation deploy --template-file ./$ECSCLUSTER_STACKNAME --stack-name $ECSCLUSTER_STACKNAME --parameter-overrides KeyName=$KEYPAIR --tags 'Key=env Value=dev' 'Key=app Value=tqtest-ecs-hello-world' 'Key=cfn-stack Value='"$ECSCLUSTER_STACKNAME"'' --capabilities CAPABILITY_IAM --region=$REGION $PROFILESWITCH
+
+# create EC2 instance for Jenkins server from ./Jenkins cloudformation template
+aws cloudformation deploy --template-file ./$JENKINS_STACKNAME --stack-name $JENKINS_STACKNAME --parameter-overrides KeyName=$KEYPAIR --tags 'Key=env Value=dev' 'Key=app Value=jenkins-server' 'Key=cfn-stack Value='"$ECSCLUSTER_STACKNAME"'' --capabilities CAPABILITY_IAM --region=$REGION $PROFILESWITCH
+ 
+# create ECR repository
+CHECK_REPO=$(aws ecr describe-repositories $PROFILESWITCH | grep "repositoryName" | grep "$ECRREPO")
+if [[ -n $CHECK_REPO ]]; then
+	echo "Repository $ECRREPO already exists"
+else
+	aws ecr create-repository --repository-name $ECRREPO --tags 'Key=env Value=dev' 'Key=app Value=tqtest-ecs-hello-world' 'Key=cfn-stack Value='"$ECSCLUSTER_STACKNAME"'' --region=$REGION $PROFILESWITCH
+fi
